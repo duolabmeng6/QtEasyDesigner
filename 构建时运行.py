919e4e8df502ce407e1c58e36b14364e7cd2b82a
duolabmeng <1715109585@qq.com>
2022-07-14 12:04:13 +0800

    试试看

--- 构建时运行.py
+++ 构建时运行.py
@@ -1,5 +1,9 @@
 from pyefun import *
 
+路径 = 读环境变量("GITHUB_ENV")
+print(路径)
+GITHUB_ENV = 读入文本("./GITHUB_ENV")
+print("GITHUB_ENV:", GITHUB_ENV)
 VERSION = 读入文本("./VERSION")
-# print("版本:", VERSION)
-写环境变量("VERSION", VERSION)
+print("版本:", VERSION)
+写到文件(路径, GITHUB_ENV + "\n" + "VERSION=" + VERSION)
