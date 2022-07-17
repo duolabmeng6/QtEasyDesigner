895f71eb0b89acdce23d820dc715f8b12d192859
duolabmeng <1715109585@qq.com>
2022-07-17 11:40:30 +0800

    列表框组件

--- qt_esay_model/中文对照组件常量.py
+++ qt_esay_model/中文对照组件常量.py
@@ -1,3 +1,4 @@
+from 组件库.组件列表框 import 组件列表框
 from 组件库.组件单行编辑框 import 组件单行编辑框
 from 组件库.组件单选框 import 组件单选框
 from 组件库.组件复选框 import 组件复选框
@@ -5,6 +6,7 @@ from 组件库.组件富文本编辑框 import 组件富文本编辑框
 from 组件库.组件按钮 import 组件按钮
 from 组件库.组件标签 import 组件标签
 from 组件库.组件纯文本编辑框 import 组件纯文本编辑框
+
 
 class 组件数据:
     组件中英文对照 = {
@@ -16,7 +18,10 @@ class 组件数据:
         "富文本编辑框": "QTextEdit",
         "复选框": "QCheckBox",
         "单选框": "QRadioButton",
+        "列表框": "QListWidget",
     }
+
+
 # 互换键值对并且加入
 def 取组件名称中英文对照(名称):
     vv = 组件数据.组件中英文对照.copy()
@@ -27,7 +32,6 @@ def 取组件名称中英文对照(名称):
 
 def 取所有组件名称():
     return 组件数据.组件中英文对照.keys()
-
 
 
 def 通过组件名称取组件库对象(组件类型, 父容器=None):
@@ -45,6 +49,8 @@ def 通过组件名称取组件库对象(组件类型, 父容器=None):
         组件库对象 = 组件复选框(父容器)
     elif 组件类型 == "QRadioButton":
         组件库对象 = 组件单选框(父容器)
+    elif 组件类型 == "QListWidget":
+        组件库对象 = 组件列表框(父容器)
     else:
         print(f"======未匹配组件 {组件类型}")
         return None
